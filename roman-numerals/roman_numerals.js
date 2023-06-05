59248f52f7e838d20a61714e7f874bbd5377ffaa
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-05 09:31:47 +0100

    Tested the decimal to Roman algorithm on decimal units, 1 to 9

--- roman-numerals/roman_numerals.js
+++ roman-numerals/roman_numerals.js
@@ -1,5 +1,64 @@
 function convertDecimalToRoman(num) {
-  return "I";
+  const romanPatternArray = [
+    "",
+    "l",
+    "ll",
+    "lll",
+    "lm",
+    "m",
+    "ml",
+    "mll",
+    "mlll",
+    "lu",
+    "u",
+  ];
+  const romanDigitLetters = {
+    thouLower: "M",
+    thouMid: "V",
+    hundLower: "C",
+    hundMid: "D",
+    hundUpper: "M",
+    tensLower: "X",
+    tensMid: "L",
+    tensUpper: "C",
+    unitLower: "I",
+    unitMid: "V",
+    unitUpper: "X",
+  };
+
+  const unitsDecimal = num % 10;
+  const unitValue = unitsDecimal;
+  const tensDecimal = (num - unitsDecimal) % 100;
+  const tensValue = tensDecimal / 10;
+  const hundredsDecimal = (num - unitsDecimal - tensDecimal) % 1000;
+  const hundValue = hundredsDecimal / 100;
+  const thousandsDecimal = Math.floor(num / 1000);
+  const thouValue = thousandsDecimal;
+
+  const unitString = romanPatternArray[unitValue];
+  let romanUnitString = unitString
+    .replace(/l/g, romanDigitLetters.unitLower)
+    .replace(/m/g, romanDigitLetters.unitMid)
+    .replace(/u/g, romanDigitLetters.unitUpper);
+
+  const tensString = romanPatternArray[tensValue];
+  let romanTensString = tensString
+    .replace(/l/g, romanDigitLetters.tensLower)
+    .replace(/m/g, romanDigitLetters.tensMid)
+    .replace(/u/g, romanDigitLetters.tensUpper);
+
+  const hundString = romanPatternArray[hundValue];
+  let romanHundString = hundString
+    .replace(/l/g, romanDigitLetters.hundLower)
+    .replace(/m/g, romanDigitLetters.hundMid)
+    .replace(/u/g, romanDigitLetters.hundUpper);
+
+  const thouString = romanPatternArray[thouValue];
+  let romanThouString = thouString
+    .replace(/l/g, romanDigitLetters.thouLower)
+    .replace(/m/g, romanDigitLetters.thouMid)
+    .replace(/u/g, romanDigitLetters.thouUpper);
+  return romanThouString + romanHundString + romanTensString + romanUnitString;
 }
 
 module.exports = convertDecimalToRoman;
--- roman-numerals/roman_numerals.test.js
+++ roman-numerals/roman_numerals.test.js
@@ -4,4 +4,14 @@ describe("Test Roman Numerals", () => {
   test("Check the pipes", () => {
     expect(convertDecimalToRoman(1)).toBe("I");
   });
+  test("Check the roman generating algorithm for base 10 units", () => {
+    expect(convertDecimalToRoman(2)).toBe("II");
+    expect(convertDecimalToRoman(3)).toBe("III");
+    expect(convertDecimalToRoman(4)).toBe("IV");
+    expect(convertDecimalToRoman(5)).toBe("V");
+    expect(convertDecimalToRoman(6)).toBe("VI");
+    expect(convertDecimalToRoman(7)).toBe("VII");
+    expect(convertDecimalToRoman(8)).toBe("VIII");
+    expect(convertDecimalToRoman(9)).toBe("IX");
+  });
 });
